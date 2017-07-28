bf41f6d3998d87003695e8241e85a91c3ac919d7
blandw <blandw@synnex.com>
2017-07-28 13:45:27 +0800

    add rest api

---
+++ initial/src/main/java/com/docker/controller/Rest.java
@@ -0,0 +1,21 @@
+package com.docker.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *@author Bland W
+ */
+
+@RestController
+public class Rest {
+
+	@RequestMapping(value="api/print")
+	@ResponseBody
+	public String print(String id){
+		return id;
+	}
+	
+}
